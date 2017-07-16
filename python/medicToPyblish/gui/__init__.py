b40e7e36353e3a268162038eb919b38cb03292ec
Sol Kim <defreturnnone@gmail.com>
2017-07-16 23:08:20 +0900

    fix ui for medic 1.0.0

--- python/medicToPyblish/gui/__init__.py
+++ python/medicToPyblish/gui/__init__.py
@@ -1,7 +1,6 @@
 from Qt import QtWidgets
 from maya import OpenMaya
-from medic.core import parameter
-from medic.core import testerBase
+import medic
 from pyblish_lite import model
 from pyblish_lite import util
 import re
@@ -14,97 +13,87 @@ class PyblishFunction():
 
     @staticmethod
     def IsTesterPlugin(plugin):
-        return hasattr(plugin, "Tester") and hasattr(plugin, "Nodes")
+        return hasattr(plugin, "Tester") and hasattr(plugin, "Reports")
+
+    @staticmethod
+    def RemoveReport(plugin, report):
+        for i, r in enumerate(plugin.Reports):
+            if report == r:
+                plugin.Reports.pop(i)
+                return True
+
+        return False
 
 
 class MayaFunction():
-    @staticmethod
-    def Select(node_components):
-        sel = OpenMaya.MSelectionList()
-        for node, component in node_components:
-            if node.IsDagNode():
-                if component is not None:
-                    sel.add(node.getPath(), component)
-                else:
-                    sel.add(node.getPath())
-            else:
-                sel.add(node.object())
-
-        OpenMaya.MGlobal.setActiveSelectionList(sel)
-
+    SelectionList = OpenMaya.MSelectionList()
     @staticmethod
     def DeselectAll():
-        OpenMaya.MGlobal.setActiveSelectionList(OpenMaya.MSelectionList())
+        OpenMaya.MGlobal.setActiveSelectionList(MayaFunction.SelectionList)
 
 
 class ParameterFunctions():
     @staticmethod
-    def GetParmeterParser(pram_dict):
-        params = []
+    def SetParmeterValue(param_container, pram_dict):
         for prm in pram_dict:
             if prm["function"]:
-                prm["function"](prm["parameter"], prm["widget"])
-                params.append(prm["parameter"])
-
-        return parameter.ParameterParser(params)
+                prm["function"](param_container, prm["name"], prm["widget"])
 
     @staticmethod
-    def SetInt(param, widget):
+    def SetInt(param, name, widget):
         t = widget.text()
         if not t:
             t = 0
-        param.set(int(t))
+        param.set(name, int(t))
 
     @staticmethod
-    def SetFloat(param, widget):
+    def SetFloat(param, name, widget):
         t = widget.text()
         if not t:
             t = 0
-        param.set(float(t))
+        param.set(name, float(t))
 
     @staticmethod
-    def SetBool(param, widget):
-        param.set(widget.isChecked())
+    def SetBool(param, name, widget):
+        param.set(name, widget.isChecked())
 
     @staticmethod
-    def SetString(param, widget):
-        param.set(str(widget.text()))
+    def SetString(param, name, widget):
+        param.set(name, str(widget.text()))
         
     @staticmethod
-    def CreateWidget(param):
-        parm_type = param.getType()
+    def CreateWidget(info):
+        name, label, parm_type, default = info
 
-        if parm_type is parameter.MdNull or\
-           parm_type is parameter.MdBoolArray or\
-           parm_type is parameter.MdIntArray or\
-           parm_type is parameter.MdFloatArray or\
-           parm_type is parameter.MdStringArray or\
-           parm_type is parameter.MdMObject or\
-           parm_type is parameter.MdMObjectArray:
+        if parm_type is medic.Types.Null or\
+           parm_type is medic.Types.BoolArray or\
+           parm_type is medic.Types.IntArray or\
+           parm_type is medic.Types.FloatArray or\
+           parm_type is medic.Types.StringArray:
             print "This type parameter is not supported yet : %s" % parm_type
             return None, None
 
         widget = None
         function = None
 
-        if parm_type == parameter.MdBool:
+        if parm_type == medic.Types.Bool:
             widget = QtWidgets.QCheckBox()
-            widget.setChecked(param.getDefault())
+            widget.setChecked(default)
             function = ParameterFunctions.SetBool
 
-        elif parm_type == parameter.MdInt:
+        elif parm_type == medic.Types.Int:
             widget = NumericLine.CreateIntLine()
-            widget.setText(str(param.getDefault()))
+            widget.setText(str(default))
             function = ParameterFunctions.SetInt
 
-        elif parm_type == parameter.MdFloat:
+        elif parm_type == medic.Types.Float:
             widget = NumericLine.CreateFloatLine()
-            widget.setText(str(param.getDefault()))
+            widget.setText(str(default))
             function = ParameterFunctions.SetFloat
 
-        elif parm_type == parameter.MdString:
+        elif parm_type == medic.Types.String:
             widget = QtWidgets.QLineEdit()
-            widget.setText(param.getDefault())
+            widget.setText(default)
             function = ParameterFunctions.SetString
 
         return widget, function
@@ -135,24 +124,23 @@ class NumericLine(QtWidgets.QLineEdit):
         e.__regex = NumericLine.RegexFloat
         return e
 
-class NodeItem(QtWidgets.QListWidgetItem):
-    def __init__(self, node, component, parent=None):
-        super(NodeItem, self).__init__(parent)
-        self.__node = node
-        self.__components = component
-        self.setText(node.name())
+class ReportItem(QtWidgets.QListWidgetItem):
+    def __init__(self, report, parent=None):
+        super(ReportItem, self).__init__(parent)
+        self.__report = report
+        self.setText(report.node().name())
 
-    def nodeAndComponents(self):
-        return self.__node, self.__components
+    def report(self):
+        return self.__report
 
 
-class NodeList(QtWidgets.QListWidget):
+class RportList(QtWidgets.QListWidget):
     def __init__(self, parent=None):
-        super(NodeList, self).__init__(parent)
+        super(RportList, self).__init__(parent)
         self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
 
-    def addNode(self, node_comp):
-        self.addItem(NodeItem(*node_comp))
+    def addReport(self, report):
+        self.addItem(ReportItem(report))
 
     def mousePressEvent(self, event):
         self.clearSelection()
@@ -170,12 +158,13 @@ class TesterDetailWidget(QtWidgets.QWidget):
         self.__docked = False
         self.__plugin = None
         self.__params = []
+        self.__param_container = None
 
         self.__qt_top_layout = None
         self.__qt_parameter_layout = None
         self.__qt_bottom_layout = None
         self.__qt_test_label = None
-        self.__qt_node_list = None
+        self.__qt_report_list = None
         self.__qt_select_button = None
         self.__qt_fix_selected_button = None
         self.__qt_fix_all_button = None
@@ -215,7 +204,7 @@ class TesterDetailWidget(QtWidgets.QWidget):
         else:
             plugin = PyblishFunction.GetPlugin(current)
             if PyblishFunction.IsTesterPlugin(plugin):
-                if plugin.Nodes:
+                if plugin.Reports:
                     self.setPlugin(plugin)
                     self.show()
 
@@ -232,19 +221,20 @@ class TesterDetailWidget(QtWidgets.QWidget):
     def setPlugin(self, plugin):
         self.__plugin = plugin
         self.setTester(plugin.Tester)
-        self.setNodes(plugin.Nodes)
+        self.setReports(plugin.Reports)
 
     def setTester(self, tester):
-        self.__setTesterName(tester.name())
-        self.__setDescription(tester.description())
+        self.__setTesterName(tester.Name())
+        self.__setDescription(tester.Description())
         self.__clearParameters()
         self.__setParameters(tester.GetParameters())
         self.__setFixable(tester.IsFixable())
 
-    def setNodes(self, nodes):
-        self.__qt_node_list.clear()
-        for node in sorted(nodes, key=lambda x: x[0].name()):
-            self.__qt_node_list.addNode(node)
+    def setReports(self, reports):
+        self.__qt_report_list.clear()
+
+        for report in sorted(reports, key=lambda x: x.node().name()):
+            self.__qt_report_list.addReport(report)
 
     def __createWidgets(self):
         main_layout = QtWidgets.QVBoxLayout()
@@ -272,10 +262,10 @@ class TesterDetailWidget(QtWidgets.QWidget):
         self.__qt_description.setMaximumHeight(75)
         self.__qt_description.setReadOnly(True)
         
-        self.__qt_node_list = NodeList()
+        self.__qt_report_list = RportList()
         self.__qt_top_layout.addWidget(self.__qt_tester_label)
         self.__qt_top_layout.addWidget(self.__qt_description)
-        self.__qt_top_layout.addWidget(self.__qt_node_list)
+        self.__qt_top_layout.addWidget(self.__qt_report_list)
 
         # bottom
         self.__qt_select_button = QtWidgets.QPushButton("Select")
@@ -295,7 +285,7 @@ class TesterDetailWidget(QtWidgets.QWidget):
 
     def __clear(self):
         self.__plugin = None
-        self.__qt_node_list.clear()
+        self.__qt_report_list.clear()
         self.__setTesterName("<UNKNOWN TESTER>")
         self.__setFixable(False)
         self.__setDescription("<DESCRIPTION>")
@@ -328,60 +318,62 @@ class TesterDetailWidget(QtWidgets.QWidget):
 
     def __clearParameters(self):
         self.__params = []
+        self.__param_container = None
         self.__clearLayout(self.__qt_parameter_layout)
 
-    def __setParameters(self, parameters):
-        for param in parameters:
-            widget, function = ParameterFunctions.CreateWidget(param)
+    def __setParameters(self, params):
+        self.__param_container = params
+        for info in params.getParamInfos():
+            p_name, p_label, p_type, p_default = info
+            widget, function = ParameterFunctions.CreateWidget(info)
             if widget:
                 layout = QtWidgets.QHBoxLayout()
-                label = QtWidgets.QLabel(param.getLabel())
+                label = QtWidgets.QLabel(p_label)
                 layout.addWidget(label)
                 layout.addWidget(widget)
 
-                self.__params.append({"parameter": param, "widget": widget, "function": function})
+                self.__params.append({"name": p_name, "widget": widget, "function": function})
                 self.__qt_parameter_layout.addLayout(layout)
 
     def __setSelection(self):
         MayaFunction.DeselectAll()
-        items = self.__qt_node_list.selectedItems()
-        MayaFunction.Select(map(lambda x: x.nodeAndComponents(), items))
+        for item in self.__qt_report_list.selectedItems():
+            item.report().addSelection()
 
     def __fixAll(self):
-        parser = ParameterFunctions.GetParmeterParser(self.__params)
+        ParameterFunctions.SetParmeterValue(self.__param_container, self.__params)
         failed = []
-        while (self.__qt_node_list.count() != 0):
-            node_item = self.__qt_node_list.item(0)
-            node, comp = node_item.nodeAndComponents()
-            if not self.__plugin.Tester.Fix(node, comp, parser):
-                failed.append((node, comp))
+        while (self.__qt_report_list.count() != 0):
+            report_item = self.__qt_report_list.item(0)
+            if not self.__plugin.Tester.fix(report_item.report(), self.__param_container):
+                failed.append(report_item.report())
             else:
-                self.__plugin.removeNode(node, comp)
+                PyblishFunction.RemoveReport(self.__plugin, report_item.report())
 
-            t_i = self.__qt_node_list.takeItem(0)
+            t_i = self.__qt_report_list.takeItem(0)
             del t_i
 
-        for node_comp in failed:
-            self.__qt_node_list.addNode(node_comp)
+        for report in failed:
+            self.__qt_report_list.addReport(report)
 
     def __fixSelected(self):
-        parser = ParameterFunctions.GetParmeterParser(self.__params)
+        ParameterFunctions.SetParmeterValue(self.__param_container, self.__params)
         indices = []
-        node_comps = []
-        for node_item in self.__qt_node_list.selectedItems():
-            node, comp = node_item.nodeAndComponents()
-            if self.__plugin.Tester.Fix(node, comp, parser):
-                indices.append(self.__qt_node_list.indexFromItem(node_item).row())
-                node_comps.append((node, comp))
+        reports = []
+        for report_item in self.__qt_report_list.selectedItems():
+            report = report_item.report()
+            if self.__plugin.Tester.fix(report, self.__param_container):
+                indices.append(self.__qt_report_list.indexFromItem(report_item).row())
+                reports.append(report)
 
         indices.sort()
         indices.reverse()
         for i in indices:
-            t_i = self.__qt_node_list.takeItem(i)
-            self.__plugin.removeNode(node, comp)
+            t_i = self.__qt_report_list.takeItem(i)
             del t_i
-        for node, comp in node_comps:
-            self.__plugin.removeNode(node, comp)
+
+        for report in reports:
+            PyblishFunction.RemoveReport(self.__plugin, report)
 
 
 class ArtistViewSignalOverride():
